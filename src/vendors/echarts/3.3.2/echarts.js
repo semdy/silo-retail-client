5183bcf9d9d6fd4e23e55be9eef70c80c4542764
semdy <501859275@qq.com>
2017-02-08 13:34:52 +0800

    认证流程优化

--- src/vendors/echarts/3.3.2/echarts.js
+++ src/vendors/echarts/3.3.2/echarts.js
@@ -1,5 +1,5 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {alert(1);
+    if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define([], factory);
     } else if (typeof module === 'object' && module.exports) {
