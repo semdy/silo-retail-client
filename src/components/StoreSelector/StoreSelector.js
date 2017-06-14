746bacbf301d806086d055b6b015689f0285b39e
semdy <501859275@qq.com>
2017-06-14 11:49:20 +0800

    UI细节调整

--- src/components/StoreSelector/StoreSelector.js
+++ src/components/StoreSelector/StoreSelector.js
@@ -80,46 +80,46 @@ class Page extends React.Component {
     let {showStore, storeList, storeSelectorTitle} = this.state;
     return (
       storeList.length === 0 ? <noscript></noscript> :
-      <div className="store-container"
-           onTouchMove={this.handleTouch}
-           onTouchEnd={this.handleTouch}
-      >
-        <Animate
-          className="backdrop"
-          transitionName="fade"
-          visible={showStore}
-          onClick={actions.hideStoreSelector}
+        <div className="store-container"
+             onTouchMove={this.handleTouch}
+             onTouchEnd={this.handleTouch}
         >
-        </Animate>
-        <Animate transitionName="popup" className="store-selector" visible={showStore}>
-          <h4 className="store-header">{storeSelectorTitle}</h4>
-          <ul className="store-list">
-            {
-              storeList.map((item, index) => {
-                return (
-                  <li key={index}
-                      onClick={this.handleItemClick.bind(this, index)}
-                      className={classnames({selected: item.selected})}
-                  >
-                    <Icon name="check" width={18} height={18}/>
-                    <span>{item.name}</span>
-                  </li>
-                )
-              })
-            }
-          </ul>
-          <div className="store-actions t-PL16 t-PR16">
-            <HBox>
-              <Box flex={1} className="t-PR8">
-                <Button type="minor" onClick={this.handleCancel.bind(this)}>{locale.cancel}</Button>
-              </Box>
-              <Box flex={1} className="t-PL8">
-                <Button type="primary" onClick={this.handleConfirm.bind(this)}>{locale.ok}</Button>
-              </Box>
-            </HBox>
-          </div>
-        </Animate>
-      </div>
+          <Animate
+            className="backdrop"
+            transitionName="fade"
+            visible={showStore}
+            onClick={actions.hideStoreSelector}
+          >
+          </Animate>
+          <Animate transitionName="popup" className="store-selector" visible={showStore}>
+            <h4 className="store-header">{storeSelectorTitle}</h4>
+            <ul className="store-list">
+              {
+                storeList.map((item, index) => {
+                  return (
+                    <li key={index}
+                        onClick={this.handleItemClick.bind(this, index)}
+                        className={classnames({selected: item.selected})}
+                    >
+                      <Icon name="check" width={18} height={18}/>
+                      <span>{item.name}</span>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+            <div className="store-actions t-PL16 t-PR16">
+              <HBox>
+                <Box flex={1} className="t-PR8">
+                  <Button type="minor" onClick={this.handleCancel.bind(this)}>{locale.cancel}</Button>
+                </Box>
+                <Box flex={1} className="t-PL8">
+                  <Button type="primary" onClick={this.handleConfirm.bind(this)}>{locale.ok}</Button>
+                </Box>
+              </HBox>
+            </div>
+          </Animate>
+        </div>
     );
   }
 }
